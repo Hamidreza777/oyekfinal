12d398dfcf70f5a463a090454aec2f6b6c0559fc
artin <m.malekii2003@gmail.com>
2023-04-25 16:43:41 +0430

    edit-artin

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import Aboutecss from '../About/About.module.css'
 
 const About = () => {
     return (
-        <div>
-             <h1>سلام این یک تست است</h1>
+       <>
+       
+       <div id={Aboutecss.contaner}>
+ 
+   <div id={Aboutecss.div2}>
+   <h1 id={Aboutecss.hh}>اعضای تیم اُیک</h1>
+   <b id={Aboutecss.textb}>اُیک توانسته با گرد هم آوری افراد متخصص در زمنیه طراحی سایت و سئو خدمات جامع و کاملی را با بهترین کیفیت به شما ارائه دهد.</b>
+   </div>
+   <div id={Aboutecss.div3}>
+   <p>salam</p>
+   </div>
        </div>
+       
+       </>
     );
 }
 
