e2509ed373970f6fd981f30b090a8ef2cf366b58
Arshia <83arshiarezaie83@gmail.com>
2023-04-25 05:12:07 -0700

    amoozesh

---
+++ src/Components/CmAmozesh/CallMain.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ItemBodySite from './BodySite/BodySite';
+import FooterItem from './Footer/Footer';
+
+const AllCode = () =>{
+
+    return(
+        <div>
+            <ItemBodySite/>
+            <FooterItem/>
+        </div>
+    )
+};
+export default AllCode;
