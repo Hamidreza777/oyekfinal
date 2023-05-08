8b4c9c311b3443fb5ad8742b9cbf6ab238200f49
Artin <m.malekii2003@gmail.com>
2023-05-08 14:11:10 +0430

    eadit

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -22,6 +22,56 @@ const Header = () => {
             <a id={Headercss.phone} href="tel:09220136476">0922-0136476</a>
           </div>
 
+<div className={Headercss.amadeh}>
+
+
+<nav  className={Headercss.menu}>
+  <input type="checkbox" href="#"  className={Headercss.menuopen} name="menu-open" id="menu-open"/>
+  <label  className={Headercss.menuopenbutton} for="menu-open">
+    <span className={`${Headercss.hamburger} ${Headercss.hamburger1}`}></span>
+    <span className={`${Headercss.hamburger} ${Headercss.hamburger2}`}></span>
+    <span className={`${Headercss.hamburger} ${Headercss.hamburger3}`}></span>
+  </label>
+  
+  <a href="#" className={`${Headercss.menuitem}`}></a>
+  <a href="#" className={`${Headercss.menuitem}`}> </a>
+  <a href="#" className={`${Headercss.menuitem}`}> </a>
+  <a href="#" className={`${Headercss.menuitem}`}>  </a>
+  <a href="#" className={`${Headercss.menuitem}`}>  </a>
+
+</nav>
+
+
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+    <defs>
+      <filter id="shadowed-goo">
+          
+          <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10" />
+          <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
+          <feGaussianBlur in="goo" stdDeviation="3" result="shadow" />
+          <feColorMatrix in="shadow" mode="matrix" values="0 0 0 0 0  0 0 0 0 0  0 0 0 0 0  0 0 0 1 -0.2" result="shadow" />
+          <feOffset in="shadow" dx="1" dy="1" result="shadow" />
+          <feBlend in2="shadow" in="goo" result="goo" />
+          <feBlend in2="goo" in="SourceGraphic" result="mix" />
+      </filter>
+      <filter id="goo">
+          <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10" />
+          <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
+          <feBlend in2="goo" in="SourceGraphic" result="mix" />
+      </filter>
+    </defs>
+</svg>
+
+
+
+
+
+
+
+</div>
+
+
+
 
 
          
@@ -121,46 +171,6 @@ const Header = () => {
 
 
 
-
-
-
-
-<nav  className={Headercss.menu}>
-  <input type="checkbox" href="#"  className={Headercss.menuopen} name="menu-open" id="menu-open"/>
-  <label  className={Headercss.menuopenbutton} for="menu-open">
-    <span className={`${Headercss.hamburger} ${Headercss.hamburger1}`}></span>
-    <span className={`${Headercss.hamburger} ${Headercss.hamburger2}`}></span>
-    <span className={`${Headercss.hamburger} ${Headercss.hamburger3}`}></span>
-  </label>
-  
-  <a href="#" className={`${Headercss.menuitem}`}> <i class="fa fa-bar-chart"></i> </a>
-  <a href="#" className={`${Headercss.menuitem}`}> <i class="fa fa-plus"></i> </a>
-  <a href="#" className={`${Headercss.menuitem}`}> <i class="fa fa-heart"></i> </a>
-  <a href="#" className={`${Headercss.menuitem}`}> <i class="fa fa-envelope"></i> </a>
-  <a href="#" className={`${Headercss.menuitem}`}> <i class="fa fa-cog"></i> </a>
-
-</nav>
-
-
-<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
-    <defs>
-      <filter id="shadowed-goo">
-          
-          <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10" />
-          <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
-          <feGaussianBlur in="goo" stdDeviation="3" result="shadow" />
-          <feColorMatrix in="shadow" mode="matrix" values="0 0 0 0 0  0 0 0 0 0  0 0 0 0 0  0 0 0 1 -0.2" result="shadow" />
-          <feOffset in="shadow" dx="1" dy="1" result="shadow" />
-          <feBlend in2="shadow" in="goo" result="goo" />
-          <feBlend in2="goo" in="SourceGraphic" result="mix" />
-      </filter>
-      <filter id="goo">
-          <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10" />
-          <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
-          <feBlend in2="goo" in="SourceGraphic" result="mix" />
-      </filter>
-    </defs>
-</svg>
 
 
 
